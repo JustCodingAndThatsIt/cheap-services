72eb997b1e7264e3dc82a9d75e233de4679bc965
JustCodingAndThatsIt <129399314+JustCodingAndThatsIt@users.noreply.github.com>
2023-04-16 18:26:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "mywebsiteonly9@gmail.com",
+        Password : "",
+        To : 'mywebsiteonly9@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
